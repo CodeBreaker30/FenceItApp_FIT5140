154f4b54edde0b08e5b2a970ceea53d38305e8fc
Alfredo Zamudio <alfredozamudio_deb@alfredos-macbook-pro.local>
2018-10-26 23:11:33 +1100

    Initial Commit

---
+++ FenceItApp_FIT5140/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FenceItApp_FIT5140
+//
+//  Created by Alfredo Zamudio on 26/10/18.
+//  Copyright © 2018 Alfredo Zamudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
